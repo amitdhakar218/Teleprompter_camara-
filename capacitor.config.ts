3d2b12177e3acb6865454d8bd9b95c0f2c3f8424
Amit Dhakar <amitdhakar218@gmail.com>
2026-09-25 20:34:38 +0530

    Add Capacitor Android app and automatic APK build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.amitdhakar.teleprompter',
+  appName: 'Teleprompter Camera',
+  webDir: 'dist'
+};
+
+export default config;
